9ca92df3e7c33af212e2fa452f0af4a9c40b3e0a
Thirumurugan-KS <thiruks.dev@gmail.com>
2021-12-10 17:37:34 +0530

    commit

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const express = require("express")
+const fileUpload = require("express-fileupload")
+const cors = require("cors")
+const cookieParser = require("cookie-parser")
+
 
 const app = express()
+
+app.use(express.json())
+app.use(cors())
+app.use(cookieParser())
+
+
 
 const User = require("./routers/userRouter")
 
